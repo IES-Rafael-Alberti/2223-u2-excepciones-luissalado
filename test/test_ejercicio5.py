c624204fa48fec8449c9ad094bdfe2131fbe0650
lsalmor <lsalmor@g.educaand.es>
2023-10-25 11:07:11 +0200

    commmit

--- test/test_ejercicio5.py
+++ test/test_ejercicio5.py
@@ -11,11 +11,11 @@ def test_verificar_contrasena_correcta():
     contrasena_ingresada = "contrasena"
     try:
         verificar_contrasena(contrasena_ingresada, contrasena_correcta)
-    except NameError as e:
-        pytest.fail(f"Se lanzó una excepción de manera incorrecta: {e}")
+    except NameError:
+        pytest.fail(f"Se lanzo una excepcion de manera incorrecta")
 
 def test_verificar_contrasena_incorrecta():
-    contrasena_correcta = "contrasena123"
+    contrasena_correcta = "contrasena"
     contrasena_ingresada = "contrasena456"
     with pytest.raises(NameError, match="Incorrect Password!!"):
         verificar_contrasena(contrasena_ingresada, contrasena_correcta)
